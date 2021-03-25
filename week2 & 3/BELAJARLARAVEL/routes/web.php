af21bc6adc465d57376196969cff3de0af33351b
Moch. Fahrony <72066155+fahrony03@users.noreply.github.com>
2021-03-25 16:56:42 +0700

    25 Maret 2021
    Membuat Controller

--- week2 & 3/BELAJARLARAVEL/routes/web.php
+++ week2 & 3/BELAJARLARAVEL/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user',['ManagementUserController@index']);
+// Route::resource('user','app/Http/Controllers/ManagementUserController@index');
